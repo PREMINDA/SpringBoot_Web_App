af3d00f125d9cd73cc433a883cd1810f685f8c0e
PREMINDA <premindabandara0777@gmail.com>
2021-06-14 01:05:19 +0530

    first commit

---
+++ src/test/java/com/example/SpringBoot_Web_App/SpringBootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_Web_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
